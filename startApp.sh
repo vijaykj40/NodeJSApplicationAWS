fe1febaa8150f0cfbaa1cf2bc19a6aa642523115
vijaykj40 <59810915+vijaykj40@users.noreply.github.com>
2020-12-30 14:50:17 -0800

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
-export DB_PRD_USER=dellstoreaws
-export DB_PRD_PASS=dellstoreaws
+export DB_PRD_HOST=dellstore2.c6m4qtw3t1gc.us-west-1.rds.amazonaws.com
+export DB_PRD_USER=dellstoreAWS
+export DB_PRD_PASS=test12324
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
